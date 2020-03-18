07206d19fb91708c1ac4700bc61cc8de282f8ba4
Hurricane0 <nikitadv777@gmail.com>
2020-03-18 17:21:17 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    //Helpers to handle promise result
+    const _onInit = auth2 => {
+      console.log("Initialization Success", auth2);
+    };
+    const _onError = error => {
+      console.log("Initialization Error", error);
+    };
+
+    //Initialization
+    window.gapi.load("auth2", () => {
+      window.gapi.auth2
+        .init({
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID
+        })
+        .then(_onInit, _onError);
+    });
+  }, []);
+
+  //Sign In function
+  const signIn = () => {
+    const _signSuccess = user => {
+      setUsername(user.getBasicProfile().getName());
+    };
+    const _signError = () => console.log("Auth Error");
+    const GoogleAuth = window.gapi.auth2.getAuthInstance();
+    GoogleAuth.signIn({ scope: "profile email" }).then(
+      _signSuccess,
+      _signError
+    );
+  };
+  //Sign Out function
+  const signOut = () => {
+    const GoogleAuth = window.gapi.auth2.getAuthInstance();
+    GoogleAuth.signOut().then(() => {
+      setUsername(null);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Google Authorization</h1>
+
+      {username ? (
+        <>
+          <p>
+            Hello, <strong>{username}</strong>!
+          </p>
+          <span onClick={signOut}>Sign Out</span>
+        </>
+      ) : (
+        <span onClick={signIn}>Sign In</span>
+      )}
     </div>
   );
-}
+};
 
 export default App;
